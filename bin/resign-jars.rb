59607347685eaa9e6e9c848eeef934c664a3fcda
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-09-24 15:17:27 -0400

    treat jars and libraries differently when resigning

--- bin/resign-jars.rb
+++ bin/resign-jars.rb
@@ -2,7 +2,8 @@
 
 require 'yaml'
 
-manifest_path = File.expand_path('../manifest', __FILE__)
+library_manifest_path = File.expand_path('../manifest-library', __FILE__)
+jar_manifest_path = File.expand_path('../manifest-jar', __FILE__)
 
 begin
   config = YAML.load_file(File.expand_path('../sign.yml',  __FILE__))
@@ -12,14 +13,19 @@ begin
   jars.each do |jar_path|
     path = File.dirname(jar_path)
     name = File.basename(jar_path)
+    library = name[/-nar(__V.*?|)\.jar/]
     Dir.chdir(path) do
-      puts "signing and repacking: #{name}"
+      puts "signing and repacking: #{path}/#{name}"
       `zip -d #{name} META-INF/\*`
-      `pack200 --repack #{name}`
-      `jar umf #{manifest_path} #{name}`
+      if library
+        `jar umf #{library_manifest_path} #{name}`
+      else
+        `jar umf #{jar_manifest_path} #{name}`
+        `pack200 --repack #{name}`
+      end
       # `jar -i #{name}`
       `jarsigner -storepass #{config[:password]} #{name} #{config[:alias]}`
-      `pack200 #{name}.pack.gz #{name}`
+      `pack200 #{name}.pack.gz #{name}` unless library
     end
   end
 rescue Errno::ENOENT
@@ -30,7 +36,7 @@ rescue Errno::ENOENT
 
   cp bin/sign_sample.yml bin/sign.yml
 
-  and edit ...
+  and edit appropriately ...
   
   HEREDOC
 end
