0dae223c99449cab8213df227ee6c0a6114ada5c
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-10-14 00:41:22 -0400

    simplify

--- start_local_server.rb
+++ start_local_server.rb
@@ -1,8 +1,10 @@
+#!/usr/bin/env ruby
+
 require 'rubygems'
+require 'yaml'
 
 gem 'rack', '>= 1.1.0'
 require 'rack'
-
 gem 'jnlp', '>= 0.6.3'
 require 'jnlp'
 
@@ -11,13 +13,13 @@ require File.expand_path('../rack/jnlp',  __FILE__)
 JRUBY = (defined? RUBY_ENGINE and RUBY_ENGINE == 'java')
 require 'thin' unless JRUBY
 
-require 'yaml'
-config = YAML.load_file('config.yml')
-SERVER = "#{config[:host]}:#{config[:port]}"
+SERVER_ROOT = File.expand_path('..',  __FILE__)
+config = YAML.load_file(File.join(SERVER_ROOT, '/config.yml'))
+SERVER_URL = "#{config[:host]}:#{config[:port]}"
 
-PUBLIC_DIR = 'public'
+PUBLIC_DIR = File.join(SERVER_ROOT, '/public')
 
-jnlps = Dir["#{PUBLIC_DIR}/**/*.jnlp"].collect { |p| p.gsub(/^#{PUBLIC_DIR}/, SERVER)}
+jnlps = Dir["#{PUBLIC_DIR}/**/*.jnlp"].collect { |p| p.gsub(/^#{PUBLIC_DIR}/, SERVER_URL)}
 puts "\nServing jnlps locally at:\n  #{jnlps.join("\n  ")}"
 
 jnlp_app = Rack::Builder.new do
