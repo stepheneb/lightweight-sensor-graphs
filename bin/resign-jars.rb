21fdcc4af1fdd825f290ef2a806a01514d02e310
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-11-05 15:18:00 -0400

    add optional '--nosign' arg to resign_jars.rb

--- bin/resign-jars.rb
+++ bin/resign-jars.rb
@@ -6,6 +6,9 @@ library_manifest_path = File.expand_path('../manifest-library', __FILE__)
 jar_manifest_path = File.expand_path('../manifest-jar', __FILE__)
 
 regex = ARGV[0]
+if opts = ARGV[1]
+  nosign = opts == '--nosign'
+end
 
 begin
   config = YAML.load_file(File.expand_path('../sign.yml',  __FILE__))
@@ -13,13 +16,18 @@ begin
   jars = Dir["#{public_dir}/**/*.jar"]
   jars = jars.grep(/#{regex}/) if regex
   puts "processing #{jars.length} jars ..."
+  if nosign
+    action_description = 'repacking'
+  else
+    action_description = 'signing and repacking'    
+  end
   jars.each do |jar_path|
     path = File.dirname(jar_path)
     name = File.basename(jar_path)
     library = name[/-nar(__V.*?|)\.jar/]
     Dir.chdir(path) do
-      puts "signing and repacking: #{path}/#{name}"
-      `zip -d #{name} META-INF/\*`
+      puts "#{action_description}: #{path}/#{name}"
+      `zip -d #{name} META-INF/\*` unless nosign
       if library
         `jar umf #{library_manifest_path} #{name}`
       else
@@ -27,7 +35,9 @@ begin
         `pack200 --repack #{name}`
       end
       # `jar -i #{name}`
-      `jarsigner -storepass #{config[:password]} #{name} #{config[:alias]}`
+      unless nosign
+        `jarsigner -storepass #{config[:password]} #{name} #{config[:alias]}`
+      end
       `pack200 #{name}.pack.gz #{name}` unless library
     end
   end
